7fe014f8ac5b50d64e7912d2bb74a6f81e7418f8
PJ Reiniger <pj.reiniger@gmail.com>
2024-11-25 21:46:10 -0500

    Update to wpilib 2

--- generate/get_revlib_dependencies.py
+++ generate/get_revlib_dependencies.py
@@ -9,7 +9,7 @@ def get_revlib_dependencies(
     use_local_allwpilib=False,
     use_local_opencv=False,
     use_local_ni=False,
-    allwpilib_version_override="2025.1.1-beta-1",
+    allwpilib_version_override="2025.1.1-beta-2",
     opencv_version_override="2024.4.8.0-4.bcr1",
 ):
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
